eaa23e59d32164ae71d64df36295513f707097be
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2021-07-03 13:20:27 +0200

    add nuxt-buefy

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,7 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     '@nuxtjs/toast',
+    'nuxt-buefy',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
