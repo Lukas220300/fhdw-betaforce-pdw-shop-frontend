1b32d2a995ab5d57fcd0eb3eb285bd815b095e2b
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2021-07-04 19:58:50 +0200

    Add Table

---
+++ components/Table.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-table :data="data" :columns="columns" :striped="true" ></b-table>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    columns: {
+      type: Array,
+      required: true
+    },
+  }
+}
+</script>
+
