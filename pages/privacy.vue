54307c52f9959de7ab3c75a1faa93f5d6412f6ac
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2021-07-09 17:03:49 +0200

    Add privacy site

---
+++ pages/privacy.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="title is-1">Datenschutzerklärung</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Privacy"
+}
+</script>
+
+<style lang="scss">
+.title.is-1 {
+  padding: 2rem 0;
+}
+</style>
