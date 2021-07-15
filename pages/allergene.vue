96ff3543cb19c1b2669c4ea48ec87fa74dac313b
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2021-07-15 22:39:29 +0200

    Change alergens

--- pages/allergene.vue
+++ pages/allergene.vue
@@ -7,60 +7,44 @@
     </div>
     <div class="c-AllergensList">
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">A</span>Eier/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">Alle Eisorten</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">A</span>Mit Konservierungsstoff</h5>
+        <!-- <h5 class="subtitle is-5">Alle Eisorten</h5> -->
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">B</span>Erdnüsse/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">Alle Erdnusssorten</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">B</span>Mit Farbstoff</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">C</span>Fisch/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">Alle Firscharten ( u.a. Anchovis, Kaviar)</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">C</span>Mit Antioxidationsmittel</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">D</span>Glutenhaltiges Getreide/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">u.a. Weizen, Hartweizen, Roggen, Gerste, Hafer</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">D</span>Mit Geschmacksverstärker</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">E</span>Krebstiere/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">u.a. Krebs, Schrimps, Garnelen, Scampi, Hummer</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">E</span>Mit Phosphat</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">F</span>Lupine/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">u.a. Lupinenmehl, Lupinenkonzentrat, Lupinenprotein</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">F</span>Mit Süßungsmittel</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">G</span>Milch/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">Alle Milchprodukte</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">G</span>Enthält eine Penylalaninquelle</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">H</span>Schalenfrüchte (Nüsse) /-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">u.a. Mandeln, Haselnüsse, Walnüsse, Pistazien, Cashewkerne</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">H</span>Geschwefelt</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">I</span>Schwefeldioxid/Sulfite</h5>
-        <h5 class="subtitle is-5">E220 – E228 u.a. in Trockenobst, Tomatenpüree, Wein</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">I</span>Geschwärzt</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">J</span>Sellerie/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">Bleichsellerie, Knollensellerie, Staudensellerie</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">J</span>Koffeinhaltig</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">K</span>Senf/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">u.a. auch Senfsprossen, Senfpulver, Senfkörner</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">K</span>Chininhaltig</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">L</span>Sesam/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">u.a. Sesamöl, Sesammehl, Sesamsamen</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">L</span>Gewachst</h5>
       </div>
       <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">M</span>Soja/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">Alle Sorten Sojabohnen</h5>
-      </div>
-      <div class="c-AllergenListItem">
-        <h5 class="title is-5"><span class="c-AllergenListItem__index">N</span>Weichtiere/-Erzeugnisse</h5>
-        <h5 class="subtitle is-5">u.a. Schnecken, Muscheln, Austern, Tintenfisch, Calamares</h5>
+        <h5 class="title is-5"><span class="c-AllergenListItem__index">M</span>Beinhaltet Gluten</h5>
       </div>
     </div>
   </div>
@@ -86,3 +70,13 @@ export default {
 
 }
 </style>
+
+
+
+
+
+
+
+
+
+
