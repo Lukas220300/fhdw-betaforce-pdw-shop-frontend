8ae2ea225f60fa12e146af522f57bc83a7121add
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2021-08-15 12:56:22 +0200

    add first step in checkout process

--- pages/checkout/index.vue
+++ pages/checkout/index.vue
@@ -1,12 +1,203 @@
 <template>
-  <div>
-    Bestellübersicht
+  <div class="c-checkout">
+    <div class="c-steps c-steps--desktop">
+      <progress class="progress is-link" :value="percentage" max="100" />
+      <div class="c-step c-step--1">
+        Login
+      </div>
+      <div class="c-step c-step--2">
+        Lieferung
+      </div>
+      <div class="c-step c-step--3">
+        Bezahlung
+      </div>
+      <div class="c-step c-step--4">
+        Bestätigung
+      </div>
+    </div>
+
+    <div v-if="activeStep === 0" class="c-stepView c-stepView--delivery card">
+      <h3 class="title is-3">Lieferadresse</h3>
+      Lukas Schönbeck <br>
+      Auf dem Schlaage 3 <br>
+      32139 Spenge <br>
+      Deutschland <br>
+      <div class="spacer"></div>
+      <h3 class="title is-3">Rechnungsadresse</h3>
+      Gleiche wie Lieferadresse
+      <div class="spacer"></div>
+      <h3 class="title is-3">Lieferart</h3>
+      <div class="control">
+        <label class="radio">
+          <input type="radio" name="answer" checked>
+          Standardliferung innerhalb von 24 Stunden.
+        </label>
+        <label class="radio" disabled>
+          <input type="radio" name="rsvp" disabled>
+          Preium Liferung (Bald verfügbar)
+        </label>
+      </div>
+      <div v-if="checkAge" class="spacer"></div>
+      <h3 v-if="checkAge" class="title is-3">Altersüberprüfung</h3>
+      <div v-if="checkAge" class="c-ageCheck">
+        <p>Bitte gib die im Bild Makirte Prüfziffer ein:</p>
+        <input class="input" type="text" v-bind:class="{'is-success':ageCheckValidation === 1, 'is-danger':ageCheckValidation > 1}" v-model="ageCheckInput" @change="checkAgeMethod" placeholder="Prüfziffer">
+        <button class="button is-primary check-button" @click="checkAgeMethod">Überprüfen</button>
+        <div class="c-ageCheck__imageContainer">
+          <img :src="require('~/assets/img/perso-1.jpg')" alt="persoImage">
+        </div>
+      </div>
+    </div>
+    <div v-else-if="activeStep === 1" class="c-stepView card">
+      Bezahlung
+    </div>
+    <div v-else-if="activeStep === 2" class="c-stepView card">
+      Bestätigung
+    </div>
+
+    <div class="card c-navigation">
+      <button v-bind:disabled="activeStep < 1" @click="setStepActive(activeStep - 1)" class="button is-second c-backButton">Zurück</button>
+      <button v-bind:disabled="activeStep === numberOfSteps-1 || ageBlocked" @click="setStepActive(activeStep + 1)" class="button is-primary c-nextButton">Weiter</button>
+      <button v-if="activeStep === numberOfSteps-1" v-bind:disabled="ageBlocked" class="button is-primary c-finishButton">Bestellen!</button>
+    </div>
+
   </div>
 </template>
 
 <script>
+import {ref} from "@nuxtjs/composition-api";
+
 export default {
-  name: "Index"
+  name: "Index",
+  setup(){
+    const percentage = ref(25)
+    const activeStep = ref(0)
+    const ageBlocked = ref(false) // wenn alter überprüft werden muss --> zum blockieren des Checkout prozesses
+    const checkAge = ref(false) // wenn alter überprüft werden muss -> zum anzeigen des Feldes
+    const ageCheckInput = ref('')
+    const ageCheckValidation = ref(0)
+    const numberOfSteps = 3
+
+
+    const setStepActive = (index) => {
+      const newIndex = index % numberOfSteps
+      activeStep.value = newIndex
+      percentage.value = newIndex*25 + 25
+    }
+
+    const checkAgeMethod = () => {
+      if(ageCheckInput.value !== '' && ageCheckInput.value.length === 7) {
+
+        const multiplikatoren = [7,3,1]
+        let sum = 0;
+        let checkNumber = -1;
+        for(let index = 0; index < 7; index++) {
+          const currentValue = ageCheckInput.value.charAt(index)
+          const currentMultiplikator = multiplikatoren[index % 3]
+          if(index !== 6) {
+            sum = sum + currentValue*currentMultiplikator
+          } else {
+            checkNumber = currentValue*1
+          }
+        }
+
+        if((sum % 10) === checkNumber) {
+          ageCheckValidation.value = 1
+          ageBlocked.value = false
+          return;
+        }
+      }
+
+      ageCheckValidation.value = 2
+      ageBlocked.value = true
+    }
+
+    return {
+      percentage,
+      activeStep,
+      numberOfSteps,
+      ageBlocked,
+      checkAge,
+      ageCheckInput,
+      ageCheckValidation,
+      setStepActive,
+      checkAgeMethod,
+    }
+  },
 }
 </script>
 
+<style lang="scss">
+.c-checkout {
+  .spacer {
+    padding: 1rem;
+  }
+  .title {
+    margin-bottom: 1rem;
+  }
+  .progress {
+    margin-bottom: 4rem;
+  }
+  .c-steps {
+    margin-top: 2rem;
+    .c-step {
+      display: inline-block;
+      position: absolute;
+      font-weight: bold;
+      top: 2rem;
+      &--1 {
+        left: 0%;
+        margin-left: -1rem;
+      }
+      &--2 {
+        left: 25%;
+        margin-left: -2rem;
+      }
+      &--3 {
+        left: 50%;
+        margin-left: -2rem;
+      }
+      &--4 {
+        left: 75%;
+        margin-left: -2rem;
+      }
+    }
+  }
+  .c-stepView {
+    padding: 1rem;
+    &--delivery {
+      .radio {
+        display: block;
+      }
+      .radio + .radio {
+        margin-left: 0;
+      }
+      .c-ageCheck__imageContainer {
+        margin-top: 1rem;
+      }
+      .check-button {
+        margin-top: 1rem;
+      }
+    }
+  }
+  .c-navigation {
+    margin-top: 2rem;
+    padding: 1rem;
+    .c-backButton {
+      padding-left: 2rem;
+      padding-right: 2rem;
+      margin-right: 1rem;
+      margin-bottom: 1rem;
+    }
+    .c-nextButton {
+      padding-right: 2rem;
+      padding-left: 2rem;
+      margin-right: 1rem;
+    }
+    .c-finishButton {
+      padding-right: 2rem;
+      padding-left: 2rem;
+    }
+  }
+}
+</style>
