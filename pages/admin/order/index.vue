cd9b9430565e6d057719f0bee8ae4e980bc2cb0c
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2021-08-17 09:55:40 +0200

    Add change oder status

--- pages/admin/order/index.vue
+++ pages/admin/order/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="c-orders">
     <h1 class="title is-1">Bestellungen</h1>
 
     <div class="control">
@@ -21,39 +21,66 @@
       </label>
     </div>
 
-    <table class="table">
-      <thead>
-      <tr>
-        <th v-if="showId"><abbr title="Bestellnummer">Id</abbr></th>
-        <th v-if="showUser">Empfänger</th>
-        <th v-if="showStatus">Status</th>
-        <th v-if="showCreatedAt">Bestelldatum</th>
-        <th>Infos</th>
-      </tr>
-      </thead>
-      <tfoot>
-      <tr>
-        <th v-if="showId"><abbr title="Bestellnummer">Id</abbr></th>
-        <th v-if="showUser">Empfänger</th>
-        <th v-if="showStatus">Status</th>
-        <th v-if="showCreatedAt">Bestelldatum</th>
-        <th>Infos</th>
-      </tr>
-      </tfoot>
-      <tbody>
-      <tr v-for="order in orders" :key="order.id">
-        <th v-if="showId">{{ order.id }}</th>
-        <td v-if="showUser">{{ order.user }}</td>
-        <td v-if="showStatus">
-          <button class="button">{{ order.status }}</button>
-        </td>
-        <td v-if="showCreatedAt">{{ order.createdAt }}</td>
-        <td>
-          <button class="button">...</button>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+    <div class="table-container">
+      <table class="table">
+        <thead>
+        <tr>
+          <th v-if="showId"><abbr title="Bestellnummer">Id</abbr></th>
+          <th v-if="showUser">Empfänger</th>
+          <th v-if="showStatus">Status</th>
+          <th v-if="showCreatedAt">Bestelldatum</th>
+          <th>Infos</th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+          <th v-if="showId"><abbr title="Bestellnummer">Id</abbr></th>
+          <th v-if="showUser">Empfänger</th>
+          <th v-if="showStatus">Status</th>
+          <th v-if="showCreatedAt">Bestelldatum</th>
+          <th>Infos</th>
+        </tr>
+        </tfoot>
+        <tbody>
+        <tr v-for="order in orders" :key="order.id">
+          <th v-if="showId">{{ order.id }}</th>
+          <td v-if="showUser">{{ order.user }}</td>
+          <td v-if="showStatus">
+            <button class="button"
+                    v-bind:class="{'is-success':order.status === 'DELIVERED', 'is-info':order.status === 'PAYED', 'is-link':order.status === 'ORDERED'}"
+                    @click="showStatusModal(order)">{{ order.status }}
+            </button>
+          </td>
+          <td v-if="showCreatedAt">{{ order.createdAt }}</td>
+          <td>
+            <button class="button">...</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <!-- Modals start -->
+    <!-- Change status -->
+    <div class="modal" v-bind:class="{'is-active':showStatusModalFlag}">
+      <div class="modal-background" @click="closeStatusModal"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Bestellstatus Ändern</p>
+          <button class="delete" aria-label="close" @click="closeStatusModal"></button>
+        </header>
+        <section class="modal-card-body">
+          <h2 class="title is-2">Status wählen:</h2>
+          <button class="button is-link" v-bind:disabled="tempOrder.status === 'ORDERED'" @click="changeOrderStatus(tempOrder, 'ORDERED')">ORDERED</button>
+          <button class="button is-info" v-bind:disabled="tempOrder.status === 'PAYED'" @click="changeOrderStatus(tempOrder, 'PAYED')">PAYED</button>
+          <button class="button is-success" v-bind:disabled="tempOrder.status === 'DELIVERED'" @click="changeOrderStatus(tempOrder, 'DELIVERED')">DELIVERED</button>
+        </section>
+      </div>
+    </div>
+
+
+    <!-- Modals end-->
+
   </div>
 </template>
 
@@ -71,9 +98,43 @@ export default {
     const showUser = ref(true)
     const showCreatedAt = ref(true)
 
-    $axios.get('/api/orders').then((result) => {
-      orders.value = result.data
-    })
+    const loadOrders = () => {
+      $axios.get('/api/orders').then((result) => {
+        const sortedOrders = result.data
+        orders.value = sortedOrders.sort((a,b) => {
+          if(a.id === b.id) {
+            return 0
+          } else if (a.id < b.id){
+            return -1
+          }
+          else {
+            return 1
+          }
+        })
+      })
+    }
+
+
+    // modals
+    const tempOrder = ref({})
+    const showStatusModalFlag = ref(false)
+
+    const showStatusModal = (order) => {
+      tempOrder.value = order
+      showStatusModalFlag.value = true
+    }
+    const closeStatusModal = () => {
+      showStatusModalFlag.value = false
+      tempOrder.value = {}
+    }
+    const changeOrderStatus = (order, status) => {
+      $axios.patch('/api/orders/' + order.id, {status}).then(() => {
+        loadOrders()
+        closeStatusModal()
+      })
+    }
+
+    loadOrders()
 
     return {
       orders,
@@ -81,8 +142,20 @@ export default {
       showStatus,
       showUser,
       showCreatedAt,
+      tempOrder,
+      showStatusModalFlag,
+      showStatusModal,
+      closeStatusModal,
+      changeOrderStatus,
     }
   },
 }
 </script>
 
+<style lang="scss">
+.c-orders {
+  .modal-background {
+    background-color: rgb(10 10 10 / 58%);
+  }
+}
+</style>
