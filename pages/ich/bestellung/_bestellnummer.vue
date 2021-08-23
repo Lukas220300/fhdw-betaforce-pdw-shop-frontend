88c102d19d3af2fc527e915e18897080c712e97f
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2021-08-23 08:18:50 +0200

    Fix order detail vue

--- pages/ich/bestellung/_bestellnummer.vue
+++ pages/ich/bestellung/_bestellnummer.vue
@@ -2,8 +2,8 @@
   <div class="c-oderDetail">
     <div v-if="order">
       <div style="margin-bottom: 1rem;">
-        <h1 class="title is-2">Bestellung vom {{order.createdAt}}</h1>
-        <h4 class="subtitle is-4">Bestellnummer {{order.id}}</h4>
+        <h1 class="title is-2">Bestellung vom {{ order.createdAt }}</h1>
+        <h4 class="subtitle is-4">Bestellnummer {{ order.id }}</h4>
       </div>
       <div class="c-TableContainer">
         <table class="table is-striped">
@@ -25,22 +25,23 @@
           </tfoot>
           <tbody>
           <tr v-for="(item, index) in order.orderItemList" :key="index">
-            <th>{{ index+1 }}</th>
-            <td>
+            <th>{{ index + 1 }}</th>
+            <td v-if="item.productVariant">
               <div class="c-tableImage">
                 <img :src="item.productVariant.product.cover">
               </div>
             </td>
-            <td>{{item.productVariant.product.name}} {{item.productVariant.unit}} X  entry.variant.unit.amount }}</td>
-            <td style="text-align: right;"> {{item.quantity}} </td>
+            <td v-if="item.productVariant">{{ item.productVariant.product.name }}</td>
+            <td> {{ item.quantity }}</td>
           </tr>
           </tbody>
         </table>
       </div>
       <div class="c-status">
         <h4 class="subtitle is-4">Status</h4>
-        <div class="c-status__container" v-bind:class="{'c-status__ordered': order.status === 'ORDERED','c-status__payed': order.status === 'PAYED','c-status__delivered': order.status === 'DELIVERED'}">
-          {{order.status}}
+        <div class="c-status__container"
+             v-bind:class="{'c-status__ordered': order.status === 'ORDERED','c-status__payed': order.status === 'PAYED','c-status__delivered': order.status === 'DELIVERED'}">
+          {{ order.status }}
         </div>
       </div>
     </div>
@@ -52,6 +53,7 @@
 
 <script>
 import {ref, useContext} from "@nuxtjs/composition-api";
+
 const {useApi} = require("@/composable/api");
 /*
 todo problematik
@@ -61,41 +63,40 @@ ohne /api gibt einen 500er fehler..
 export default {
   name: "Bestellnummer",
   middleware: ['loggedIn'],
-  setup(){
+  setup: () => {
     const {params, $axios, app} = useContext()
     let bestellnummer = -1
-    if(params.value.bestellnummer) {
+    if (params.value.bestellnummer) {
       bestellnummer = params.value.bestellnummer
     }
-    if(bestellnummer === -1) {
+    if (bestellnummer === -1) {
       app.router.push('/ich/meine-bestellungen')
     }
 
     const order = ref()
 
-    useApi($axios).order.findOneById(bestellnummer).then((response) => {
-      order.value = response
-      /* order.value.orderItemList.forEach(async (item) => {
-        if (typeof item.productVariant.unit === 'number') {
-          const result = await useApi($axios).unit.findOneById(item.productVariant.unit)
-          console.log(result)
-        }
-      }) */ // todo fetch unit if not availabele in serialization
+    useApi($axios).order.findOneById(bestellnummer).then(async (response) => {
+      const orderApi = {...response}
 
+      const orderItemList = await $axios.$get(orderApi._links.order_item_list.href)
+      orderApi.orderItemList = orderItemList._embedded.order_items
 
-      order.value.orderItemList.forEach((item) => {
-        if (typeof item === 'number') {
-          useApi($axios).orderItem.findOneById(item).then((response) => {
-            console.log(response)
-          })
-        }
-      })
+      for (const item in orderApi.orderItemList) {
+        const productVariantApi = await $axios.$get(orderApi.orderItemList[item]._links.product_variant.href)
+        orderApi.orderItemList[item].productVariant = productVariantApi
 
-      console.log(response)
+        const productApi = (await $axios.get(orderApi.orderItemList[item].productVariant._links.product.href)).data
+        const unitApi = (await $axios.get(orderApi.orderItemList[item].productVariant._links.unit.href)).data
+        orderApi.orderItemList[item].productVariant.product = productApi
+        orderApi.orderItemList[item].productVariant.unit = unitApi
+
+      }
+
+      order.value = orderApi
     })
 
     return {
-      order
+      order,
     }
   }
 }
@@ -106,6 +107,7 @@ export default {
   table {
     width: 100%;
   }
+
   .c-tableImage {
     > img {
       object-fit: scale-down;
@@ -113,20 +115,26 @@ export default {
       height: 2rem;
     }
   }
+
   .c-TableContainer {
     overflow-x: scroll;
   }
+
   .c-status {
     margin-top: 1rem;
+
     &__ordered {
       background-color: #d2e0fb;
     }
+
     &__payed {
       background-color: #a7d6f9;
     }
+
     &__delivered {
       background-color: #91f5e6;
     }
+
     &__container {
       padding: 1rem;
       border-radius: 15px;
