8571095bccc0a0b8bc9ad727db25a54a8837cc07
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2021-08-25 10:29:49 +0200

    code style

--- pages/ueber-uns.vue
+++ pages/ueber-uns.vue
@@ -7,13 +7,16 @@
     <h5 class="subtitle is-4">euer kompetenter Partner rund um das Thema Getränke.</h5>
 
     <div>
-      <Icon name="logo" />
+      <Icon name="logo"/>
     </div>
 
-    <p class="block">Unser Slogan „Stay hydrated“ ist nicht nur ein Spruch, sondern das zentrale Element in unserem jungen Unternehmen.</p>
+    <p class="block">Unser Slogan „<strong>Stay hydrated</strong>“ ist nicht nur ein Spruch, sondern das zentrale
+      Element in unserem jungen Unternehmen.</p>
 
-    <p class="block">Wir möchten allen Bewohnern im Kreis Ostwestfalen die Möglichkeit bieten, die Getränke, die ihr euch so sehnlichst
-    wünscht bequem im Internet zu bestellen. Unsere Bestimmung ist es, euch Getränke überall wo ihr wollt hinzuliefern.
+    <p class="block">Wir möchten allen Bewohnern im Kreis Ostwestfalen die Möglichkeit bieten, die Getränke, die ihr
+      euch so sehnlichst
+      wünscht bequem im Internet zu bestellen. Unsere Bestimmung ist es, euch Getränke überall wo ihr wollt
+      hinzuliefern.
       Egal ob an einen Badesee, eine Firma oder bei einer Party bei euch zuhause.</p>
 
     <div class="block">
@@ -22,12 +25,13 @@
       <p><strong>Ja, das stimmt.</strong></p>
     </div>
 
-    <p class="block">Aber das Beste ist, dass die Getränkebestellung so einfach ist, dass sie jeder tätigen kann. Einmal in unserem Shop
-    angemeldet, so könnt ihr bequem über eure hinterlegten Daten Getränke bestellen. Egal ob Bier, Spirituosen,
-    Softdrinks oder für den exquisiteren Gaumen Champagner. Wir haben verschiedene Sorten jedes Produktes für Euch in
+    <p class="block">Aber das Beste ist, dass die Getränkebestellung so einfach ist, dass sie jeder tätigen kann. Einmal
+      in unserem Shop
+      angemeldet, so könnt ihr bequem über eure hinterlegten Daten Getränke bestellen. Egal ob Bier, Spirituosen,
+      Softdrinks oder für den exquisiteren Gaumen Champagner. Wir haben verschiedene Sorten jedes Produktes für Euch in
       unserem Sortiment.</p>
 
-    <p>Meldet euch an und „stay hydrated“ mit der Getränkelieferung von SIP.shop !</p>
+    <p>Meldet euch an und „stay hydrated“ mit der Getränkelieferung von <strong>SIP.shop !</strong></p>
 
     <div style="margin: 2rem">
       <nuxt-link to="/auth/login" class="button is-primary is-fullwidth">Jetzt Los SHOPPEN!</nuxt-link>
